66810002aae852fd46162c9a9b978d458066386f
MarioB06 <mbozic2006@gmail.com>
2025-05-20 11:28:42 +0200

    init

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("[data-include]").forEach(async (el) => {
+      const file = el.getAttribute("data-include");
+      const res = await fetch(file);
+      if (res.ok) {
+        el.innerHTML = await res.text();
+      }
+    });
+  });
+  
